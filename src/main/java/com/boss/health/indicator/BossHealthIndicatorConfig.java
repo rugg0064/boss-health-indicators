248da1e4dcdb0c92e19d4a9e5addde9b87e0616e
Rugg <colony4ever@gmail.com>
2023-07-14 13:16:22 -0500

    Change ConfigGroup from main to bosshealthindicato

--- src/main/java/com/boss/health/indicator/BossHealthIndicatorConfig.java
+++ src/main/java/com/boss/health/indicator/BossHealthIndicatorConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("main")
+@ConfigGroup("bosshealthindicator")
 public interface BossHealthIndicatorConfig extends Config {
     @ConfigItem(
             keyName = "showPanel",
